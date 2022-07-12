08752e3a0555761163c639c35e5a20ffdb72496f
srivastavakshitijprofessional@gmail.com <srivastavakshitijprofessional@gmail.com>
2022-07-12 19:55:56 +0530

    initial

---
+++ test/evenTest.js
@@ -0,0 +1,13 @@
+const Even = artifacts.require("Even");
+
+contract("Even", () => {
+  it("Should check whether a is even or not", async () => {
+    const even = await Even.deployed();
+    try {
+      const result = await even.checkEven(4);
+      assert(result.toNumber() === 4);
+    } catch (e) {
+      assert(false, "a is odd");
+    }
+  });
+});
